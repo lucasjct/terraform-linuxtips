2f29cbc8a45afecbf5aa16bae20b882f9a924d62
Projeto 22 <qa.projeto22@gmail.com>
2023-01-24 00:14:27 -0300

    add-user-aws

--- extra/aws_iam_user.tf
+++ extra/aws_iam_user.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_user" "tst" {
-  name          = "<name-user>"
+  name          = var.user_name
   path          = "/"
   force_destroy = true
 
@@ -14,4 +14,57 @@ resource "aws_iam_user_login_profile" "tst" {
   password_reset_required = true
 }
 
-// https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy_attachment
+// https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy_attachment  
+
+
+resource "aws_iam_role" "role" {
+  name = "test-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+// resource "aws_iam_group" "group" {
+//   name = "test-group"
+// }
+
+resource "aws_iam_policy" "policy" {
+  name        = "test-policy"
+  description = "A test policy"
+
+// Policy para Adminstrator Access
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  users      = [aws_iam_user.tst.name]
+  roles      = [aws_iam_role.role.name]
+  // groups     = [aws_iam_group.group.name]
+  policy_arn = aws_iam_policy.policy.arn
+}
--- extra/outputs.tf
+++ extra/outputs.tf
@@ -1,24 +1,32 @@
 output "password" {
+
+  description = "Password for user:"
   value = aws_iam_user_login_profile.tst.encrypted_password
 
 }
 
 output "arn" {
+
+  description = "ARN value. Get you id account of 12 numbers:"
   value = aws_iam_user.tst.arn
 }
 
 output "senha" {
+
+  description = "Copy e save youd password:"
   value = aws_iam_user_login_profile.tst.password
 }
 
 output "id" {
 
+  description = "Get you alias accoun:"
   value = aws_iam_user_login_profile.tst.id
 
 }
 
 output "name" {
-
+  
+  description = "Get you user name:"
   value = aws_iam_user_login_profile.tst.user
 
 }
